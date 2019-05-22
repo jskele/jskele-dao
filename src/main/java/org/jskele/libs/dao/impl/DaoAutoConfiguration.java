fc340d549281c86388ae52b617279659f82ac48d
Ats Uiboupin <ats.uiboupin@iglu.ee>
2019-05-22 14:21:30 +0300

    add default ConversionService bean

--- src/main/java/org/jskele/libs/dao/impl/DaoAutoConfiguration.java
+++ src/main/java/org/jskele/libs/dao/impl/DaoAutoConfiguration.java
@@ -1,10 +1,19 @@
 package org.jskele.libs.dao.impl;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
 
 @Configuration
 @ComponentScan
 public class DaoAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    ConversionService conversionService() {
+        return new DefaultConversionService();
+    }
 
 }
--- src/test/java/org/jskele/libs/Application.java
+++ src/test/java/org/jskele/libs/Application.java
@@ -1,15 +1,7 @@
 package org.jskele.libs;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.support.DefaultConversionService;
 
 @SpringBootApplication
 public class Application {
-
-    @Bean
-    ConversionService conversionService() {
-        return new DefaultConversionService();
-    }
 }
