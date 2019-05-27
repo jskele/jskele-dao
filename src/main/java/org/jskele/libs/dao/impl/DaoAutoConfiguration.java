1eccc64ca37f9c4aa57a944e57b5488ccbf2b16a
Meelis Lehtmets <meelis@lehtmets.com>
2019-05-27 21:29:02 +0300

    cleanup: custom ConversionService is no longer required

--- src/main/java/org/jskele/libs/dao/impl/DaoAutoConfiguration.java
+++ src/main/java/org/jskele/libs/dao/impl/DaoAutoConfiguration.java
@@ -2,22 +2,15 @@ package org.jskele.libs.dao.impl;
 
 import org.jskele.libs.dao.Dao;
 import org.jskele.libs.dao.DbSchemaResolver;
-import org.jskele.libs.dao.impl.mappers.LibDaoConversionService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.core.convert.ConversionService;
 
 @Configuration
 @ComponentScan
 public class DaoAutoConfiguration {
-    @Bean
-    @ConditionalOnMissingBean
-    ConversionService conversionService() {
-        return new LibDaoConversionService();
-    }
 
     @Bean
     @ConditionalOnMissingBean
